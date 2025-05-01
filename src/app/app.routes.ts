231c1ff9d57783faca1aacaf2fae19302a9e60d0
Manikandan1368 <manikandanangdev68@gmail.com>
2025-05-02 05:17:41 +0530

    tavleview

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { RouterModule, Routes } from '@angular/router';
+import { TableviewComponent } from './tableview/tableview.component';
+
+export const routes: Routes = [
+    { path: '', component: TableviewComponent },
+];
